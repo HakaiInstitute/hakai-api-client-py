3bbe7a7c8564d4f98173622e76cd9ec901989570
Taylor Denouden <taylor.denouden@hakai.org>
2017-06-29 12:06:35 -0700

    Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hakai_api',
-      version='0.1',
+      version='0.1.1',
       description='Get Hakai database resources with http calls',
       url='https://github.com/tayden/hakai-api-client-python',
       author='Taylor Denouden',
