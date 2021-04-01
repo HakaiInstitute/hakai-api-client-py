817a4ae51a6a4dcffb97700471154b1b9f665355
Taylor Denouden <taylordenouden@gmail.com>
2021-04-01 10:25:35 -0700

    Update example so it works

--- example.py
+++ example.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
 """An example showing how to use the Hakai Api Python Client."""
 
 from hakai_api import Client
 
+if __name__ == '__main__':
+    # Get the api request client
+    client = Client()
 
-# Get the api request client
-client = Client()
+    # Make a data request for sampling stations
+    url = '%s/%s' % (client.api_root, '/aco/views/projects/phases')
+    response = client.get(url)
 
-# Make a data request for sampling stations
-url = '%s/%s' % (client.api_root, 'eims/views/station_matrix?limit=-1&fields=project_name,project_pk&distinct')
-response = client.get(url)
-
-print(url)
-print(response.json())
+    print(url)
+    print(response.json())
