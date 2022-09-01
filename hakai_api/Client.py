9a1a5ec7ec86f0a04b0511b303a219dba74d7ace
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2022-09-01 12:37:58 -0400

    add credentials input which can be either string or dictionnary

--- hakai_api/Client.py
+++ hakai_api/Client.py
@@ -15,21 +15,33 @@ from requests_oauthlib import OAuth2Session
 
 
 class Client(OAuth2Session):
-    _credentials_file = os.path.expanduser('~/.hakai-api-auth')
+    _credentials_file = os.path.expanduser("~/.hakai-api-auth")
 
-    def __init__(self, api_root: str = "https://hecate.hakai.org/api",
-                 login_page: str = "https://hecate.hakai.org/api-client-login"):
+    def __init__(
+        self,
+        api_root: str = "https://hecate.hakai.org/api",
+        login_page: str = "https://hecate.hakai.org/api-client-login",
+        credentials: Union[str,Dict] = None,
+    ):
         """Create a new Client class with credentials.
 
         Params:
             api_root: The base url of the hakai api you want to call.
                       Defaults to the production server.
-        """
+            credentials (str, Dict): Credentials token retrieved from 
+                                     the hakai api login page.
+                                     Defaults to the cached credentials or prompt
+        """     
         self._api_root = api_root
         self._authorization_base_url = login_page
 
-        # Try to get cached credentials
-        credentials = self._try_to_load_credentials()
+        # Load credentials from input or file
+        if isinstance(credentials, str):
+            credentials = dict(map(lambda x: x.split("="), credentials.split("&")))
+        elif credentials is None:
+            # Try to get cached credentials
+            credentials = self._try_to_load_credentials()
+
         if credentials:
             self._credentials = credentials
         else:
@@ -59,7 +71,7 @@ class Client(OAuth2Session):
 
     def _save_credentials(self, credentials: Dict):
         """Save the credentials object to a file."""
-        with open(self._credentials_file, 'wb') as outfile:
+        with open(self._credentials_file, "wb") as outfile:
             pickle.dump(credentials, outfile)
 
     def _try_to_load_credentials(self) -> Union[Dict, bool]:
@@ -67,16 +79,20 @@ class Client(OAuth2Session):
         if not os.path.isfile(self._credentials_file):
             return False
 
-        with open(self._credentials_file, 'rb') as infile:
+        with open(self._credentials_file, "rb") as infile:
             try:
                 credentials = pickle.load(infile)
-                expires_at = int(credentials['expires_at'])
+                expires_at = int(credentials["expires_at"])
             except (KeyError, ValueError):
                 os.remove(self._credentials_file)
                 return False
 
-            now = int((mktime(datetime.now(tz=utc).timetuple()) + datetime.now(
-                tz=utc).microsecond / 1000000.0))  # utc timestamp
+            now = int(
+                (
+                    mktime(datetime.now(tz=utc).timetuple())
+                    + datetime.now(tz=utc).microsecond / 1000000.0
+                )
+            )  # utc timestamp
 
             if now > expires_at:
                 os.remove(self._credentials_file)
@@ -86,11 +102,11 @@ class Client(OAuth2Session):
 
     def _get_credentials_from_web(self) -> Dict:
         """Get user credentials from a web sign-in."""
-        print('Please go here and authorize:')
+        print("Please go here and authorize:")
         print(self._authorization_base_url)
 
         sleep(0.05)  # Workaround for input / print stream race condition
-        response = input('\nCopy and past your credentials from the login page:\n')
+        response = input("\nCopy and past your credentials from the login page:\n")
 
         # Reformat response to dict
         credentials = dict(map(lambda x: x.split("="), response.split("&")))
